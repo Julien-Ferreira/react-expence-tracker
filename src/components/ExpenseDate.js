092d8db4dfbe2c3d15cff27f18ef02c8be3b39b3
Julien Fereira <ferreira-julien@live.fr>
2022-12-19 21:31:51 +0100

    props

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,12 +1,14 @@
+
 function ExpenseDate(props) {
   const month = props.date.toLocaleString('fr-FR', { month: 'long' });
   const day = props.date.toLocaleString('fr-FR', { day: '2-digit' });
   const year = props.date.getFullYear();
+
   return (
     <div className="expense-date">
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   )
 };
